a06f35e3fd94d4ca4658878346c6fb423a93433a
Andrew Martinez <martinez.s.andrew@gmail.com>
2025-07-30 09:17:51 -0400

    Made some styling adjustments.

--- MovieApp/Models/SeedMovieData.cs
+++ MovieApp/Models/SeedMovieData.cs
@@ -15,38 +15,62 @@ public static class SeedMovieData
         {
             // Look for any movies.
             if (context.Movie.Any())
-                return; // DB has been seeded
+                return;
 
             context.Movie.AddRange(
                 new Movie
                 {
-                    Title = "When Harry Met Sally",
-                    ReleaseDate = DateTime.Parse("1989-2-12"),
-                    Genre = "Romantic Comedy",
-                    Rating = "R",
+                    Title = "How to Train Your Dragon",
+                    ReleaseDate = DateTime.Parse("2025-06-13"),
+                    Genre = "Fantasy",
+                    Rating = "PG",
                     Price = 7.99M,
                 },
                 new Movie
                 {
-                    Title = "Ghostbusters ",
-                    ReleaseDate = DateTime.Parse("1984-3-13"),
-                    Genre = "Comedy",
-                    Rating = "R",
+                    Title = "Demon Slayer: Kimetsu no Yaiba Infinity Castle",
+                    ReleaseDate = DateTime.Parse("2025-09-12"),
+                    Genre = "Animation",
+                    Rating = "PG12",
                     Price = 8.99M,
                 },
                 new Movie
                 {
-                    Title = "Ghostbusters 2",
-                    ReleaseDate = DateTime.Parse("1986-2-23"),
-                    Genre = "Comedy",
-                    Rating = "R",
+                    Title = "Lilo & Stitch",
+                    ReleaseDate = DateTime.Parse("2025-05-23"),
+                    Genre = "Family",
+                    Rating = "PG",
                     Price = 9.99M,
                 },
                 new Movie
                 {
-                    Title = "Rio Bravo",
-                    ReleaseDate = DateTime.Parse("1959-4-15"),
-                    Genre = "Western",
+                    Title = "M3GAN 2.0",
+                    ReleaseDate = DateTime.Parse("2025-06-27"),
+                    Genre = "Action",
+                    Rating = "PG-13",
+                    Price = 3.99M,
+                },
+                new Movie
+                {
+                    Title = "Happy Gilmore 2",
+                    ReleaseDate = DateTime.Parse("2025-07-25"),
+                    Genre = "Comedy",
+                    Rating = "PG-13",
+                    Price = 3.99M,
+                },
+                new Movie
+                {
+                    Title = "The Fantastic 4: First Steps",
+                    ReleaseDate = DateTime.Parse("2025-07-25"),
+                    Genre = "Science Fiction",
+                    Rating = "PG-13",
+                    Price = 3.99M,
+                },
+                new Movie
+                {
+                    Title = "Bride Hard",
+                    ReleaseDate = DateTime.Parse("2025-06-20"),
+                    Genre = "Action",
                     Rating = "R",
                     Price = 3.99M,
                 }
--- MovieApp/Models/SeedShowData.cs
+++ MovieApp/Models/SeedShowData.cs
@@ -15,26 +15,67 @@ public static class SeedShowData
         {
             // Look for any shows
             if (context.Show.Any())
-                return; // DB has been seeded
+                return;
 
             context.Show.AddRange(
                 new TelevisionShow
                 {
-                    Title = "When Harry Met Sally",
-                    ReleaseDate = DateTime.Parse("1989-2-12"),
-                    Genre = "Romantic Comedy",
-                    Rating = "R",
+                    Title = "Chespirito: Not Really on Purpose",
+                    ReleaseDate = DateTime.Parse("2025-2-12"),
+                    Genre = "Drama",
+                    Rating = "TV-MA",
                     Price = 7.99M,
                 },
                 new TelevisionShow
                 {
-                    Title = "The Dark Knight",
-                    ReleaseDate = DateTime.Parse("1989-2-12"),
-                    Genre = "Romantic Comedy",
-                    Rating = "R",
+                    Title = "Ici tout commence",
+                    ReleaseDate = DateTime.Parse("2020-1-13"),
+                    Genre = "Drama",
+                    Rating = "NR",
+                    Price = 7.99M,
+                },
+                new TelevisionShow
+                {
+                    Title = "Radio Star",
+                    ReleaseDate = DateTime.Parse("2007-12-1"),
+                    Genre = "Talk",
+                    Rating = "TV-14",
+                    Price = 7.99M,
+                },
+                new TelevisionShow
+                {
+                    Title = "Men on a Mission",
+                    ReleaseDate = DateTime.Parse("2015-7-23"),
+                    Genre = "Reality",
+                    Rating = "TV-14",
+                    Price = 7.99M,
+                },
+                new TelevisionShow
+                {
+                    Title = "Good Mythical Morning",
+                    ReleaseDate = DateTime.Parse("2012-1-17"),
+                    Genre = "Comedy",
+                    Rating = "TV-14",
+                    Price = 7.99M,
+                },
+                new TelevisionShow
+                {
+                    Title = "Squid Game",
+                    ReleaseDate = DateTime.Parse("2021-6-3"),
+                    Genre = "Action",
+                    Rating = "TV-MA",
+                    Price = 7.99M,
+                },
+                new TelevisionShow
+                {
+                    Title = "The Amazing Race",
+                    ReleaseDate = DateTime.Parse("2001-2-20"),
+                    Genre = "Reality",
+                    Rating = "TV-PG",
                     Price = 7.99M,
                 }
             );
+            context.SaveChanges();
         }
     }
 }
